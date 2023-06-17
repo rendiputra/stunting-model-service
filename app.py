a2de97b8561ed6ba4c1d12cb2f27238a53c4c863
rendiputra <rendiputrapradana@gmail.com>
2023-06-17 20:17:29 +0700

    add status berat badan

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# python -m flask run
 from flask import Flask, jsonify, request, make_response
 
 import joblib
@@ -23,4 +24,18 @@ def hello():
 
   return make_response(jsonify({'data': b}), 200)
 
+@app.route('/bb-u', methods=['POST'])
+def berat():
+  data = request.get_json()
+  umur = data['umur']
+  jk = data['jk']
+  berat = data['berat']
+
+  knn = joblib.load('knn_berat_status_gizi.model')
+  test = pd.DataFrame({'Umur (bulan)': [umur], 'Berat Badan (kg)': [berat], 'Jenis Kelamin': [jk]})
+  pred = knn.predict(test)
+  a = np.array(pred)
+  b = a.tolist()
+
+  return make_response(jsonify({'data': b}), 200)
 app.run()
